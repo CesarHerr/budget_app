0c8e8e06066e71fa23ab8f13cf4c3f3e84ef168d
Cesar Herrera <cesarherr321@gmail.com>
2023-10-15 15:02:41 -0300

    Add roots to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  resoucers :group do
+    resources :paid_up
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
