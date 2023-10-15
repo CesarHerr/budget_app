2f11a9c215139ff6ea3ebf9c9786605c77dff876
Cesar Herrera <cesarherr321@gmail.com>
2023-10-15 18:42:25 -0300

    Add action show to paid_ups controller

--- app/controllers/paid_ups_controller.rb
+++ app/controllers/paid_ups_controller.rb
@@ -1,5 +1,14 @@
 class PaidUpsController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @group = Group.find(params[:group_id])
+    @paid_up = PaidUp.find(params[:id])
+  end
+
+  private
+
+  def params_paid_up
+    params.require(:paid_up).permit(:author_id, :group_id)
+  end
 end
