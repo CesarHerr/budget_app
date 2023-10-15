3ea403dd3b1ed956da70a5e7eba974c09ecc2921
Cesar Herrera <cesarherr321@gmail.com>
2023-10-15 15:24:36 -0300

    Setup controllers

---
+++ app/controllers/paid_ups_controller.rb
@@ -0,0 +1,7 @@
+class PaidUpsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resoucers :group do
-    resources :paid_up
+  resources :groups do
+    resources :paid_ups
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
