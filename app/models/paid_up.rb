9b1921f17537c0460e55f78b065296eb25798b4c
Cesar Herrera <cesarherr321@gmail.com>
2023-10-16 19:58:16 -0300

    Add relationship with join table to paid_up

--- app/models/paid_up.rb
+++ app/models/paid_up.rb
@@ -1,6 +1,8 @@
 class PaidUp < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_and_belongs_to_many :groups, foreign_key: :group_id
+  has_and_belongs_to_many :groups, join_table: 'groups_paid_ups',
+                                   foreign_key: 'paid_up_id',
+                                   association_foreign_key: 'group_id'
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_or_equal_to: 0 }
