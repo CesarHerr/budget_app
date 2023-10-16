32ecc32e2226f4b52ab323bcad1da431b39a5325
Cesar Herrera <cesarherr321@gmail.com>
2023-10-15 23:42:21 -0300

    Add new and create actions for paid_ups controller

--- app/controllers/paid_ups_controller.rb
+++ app/controllers/paid_ups_controller.rb
@@ -6,9 +6,26 @@ class PaidUpsController < ApplicationController
     @paid_up = PaidUp.find(params[:id])
   end
 
+  def new
+    @paid_up = PaidUp.new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    @paid_up = current_user.paid_ups.new(params_paid_up)
+    @group = Group.find(params[:group_id])
+    if @paid_up.save
+      flash[:notice] = 'Transaction successfully created'
+      redirect_to group_path(@group)
+    else
+      flash[:alert] = 'Something went wrong'
+      redirect_to new_group_paid_up_path(@group)
+    end
+  end
+
   private
 
   def params_paid_up
-    params.require(:paid_up).permit(:author_id, :group_id)
+    params.require(:paid_up).permit(:author_id, :group_id, :amount, :name)
   end
 end
