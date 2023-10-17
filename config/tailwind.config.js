a057f045ed17baa22ffc30607e25b7f03eeda4da
Cesar Herrera <cesarherr321@gmail.com>
2023-10-17 13:48:48 -0300

    Set up tailwind

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,21 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        prox: ['Proxima Nova', 'sans-serif'],
+      },
+      colors: {
+        cBlue: {
+          100: '#3778c2',
+        },
+        cGreen: {
+          100: '#5fb523',
+        },
+        cBlack: {
+          100: '#434b54',
+        },
+      },
+      maxWidth: {
+        'xxs': '200px',
       },
     },
   },
